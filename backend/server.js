5b7481fb8ec719ee22c09f5fad3a258e7d3c8411
Pranush <p18chandaka@gmail.com>
2026-04-28 15:42:42 +0530

    fix: use env vars in production

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
+}
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
